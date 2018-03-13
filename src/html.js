d331813de61c51896c81f8a4d6c39b11fcd5657d
cxswow <cxswow@outlook.com>
2018-03-14 01:19:35 +0800

    修改lint规则并尽量做适配

--- src/html.js
+++ src/html.js
@@ -1,20 +1,20 @@
-import React from "react"
+import React from 'react'
 
 export default class HTML extends React.Component {
   render() {
     return (
-      <html>
+      <html lang='en'>
         <head>
-          <meta charSet="utf-8" />
+          <meta charSet='utf-8' />
           <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
+            name='viewport'
+            content='width=device-width, initial-scale=1.0'
           />
           {this.props.headComponents}
         </head>
         <body>
           <div
-            id="___gatsby"
+            id='___gatsby'
             dangerouslySetInnerHTML={{ __html: this.props.body }}
           />
           {this.props.postBodyComponents}
